4eb3e69c580c727d6726dd9dcec4b1d1e0a1a116
Robbert Fliek <Bayangannaga@hotmail.com>
2016-01-15 23:31:21 +0100

    code rush working

---
+++ HKU Game/Assets/Scripts/Edit_script.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Edit_script : MonoBehaviour {
+
+    private Player_script playScript;
+    public bool editTxt = false;
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        GameObject player = GameObject.Find("Player");
+        playScript = player.GetComponent<Player_script>();
+    }
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag == "Player")
+        {
+            GameObject coder = GameObject.Find("Code");
+            if(editTxt == true && playScript.jumpCount == 0)
+            {
+                    playScript.normal = coll.contacts[0].normal;
+                    if (playScript.normal.y < 0)
+                    {
+                        playScript.jumpCount += 2;
+                    }
+
+            }
+            else if (editTxt == true && playScript.jumpCount == 1)
+            {
+                playScript.normal = coll.contacts[0].normal;
+                if (playScript.normal.y < 0)
+                {
+                    playScript.jumpCount += 1;
+                }
+
+            }
+        }
+    }
+    void OnCollisionExit2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag == "Player")
+        {
+            playScript.normal = coll.contacts[0].normal;
+            if (playScript.normal.x > 0 || playScript.normal.x < 0)
+                playScript.sideTouch = false;
+            editTxt = false;
+        }
+    }
+
+}
---
+++ HKU Game/Assets/Scripts/Player_script.cs
@@ -0,0 +1,103 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player_script : MonoBehaviour 
+{
+
+    public float maxSpeed = 10.0F;
+    public float jumpSpeed = 15.0F;
+    //public float gravity = 0.01F;
+    bool facingRight = true;
+    private Rigidbody2D playerBody;
+    //bool grounded =false;
+    public int jumpCount = 2;
+    public Vector2 normal;
+    public bool sideTouch = false;
+    private Code_script codeScript;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        playerBody = GetComponent<Rigidbody2D>();
+        GameObject coder = GameObject.Find("Code");
+        codeScript = coder.GetComponent<Code_script>();
+        
+
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        float move = Input.GetAxis("Horizontal");
+
+        if (sideTouch == false)
+        {
+            playerBody.velocity = new Vector2(move * maxSpeed, playerBody.velocity.y);
+        }
+        
+
+ 
+        if (Input.GetButtonDown("Jump") && jumpCount >= 1)
+        {
+            playerBody.velocity = new Vector2(move * maxSpeed, jumpSpeed);
+
+            jumpCount -= 1;
+        }
+
+        if (codeScript.unCode == true)
+        {
+            if(Input.GetKeyDown(KeyCode.F))
+            {
+                codeScript.WriteCode();
+            }
+        }
+	}
+
+    void OnCollisionStay2D(Collision2D coll)
+    {
+        //on collision with sides of platform, set sideTouch to true
+        if (coll.gameObject.tag == "Platform")
+        {
+            normal = coll.contacts[0].normal;
+            if (normal.x > 0 || normal.x < 0)
+                sideTouch = true;
+        }
+
+    }
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        //if double jump was used and player has collison with platform top, add more jumpCounts
+        if (coll.gameObject.tag == "Platform" && jumpCount == 0)
+        {
+            normal = coll.contacts[0].normal;
+            if (normal.y > 0)
+            {
+                jumpCount += 2;
+            }
+
+        }
+        //if jump was used and player has collison with platform top, add 1 more jumpCount
+        else if (coll.gameObject.tag == "Platform" && jumpCount == 1)
+        {
+            normal = coll.contacts[0].normal;
+            if (normal.y > 0)
+            {
+                jumpCount += 1;
+            }
+
+        }
+
+    }
+
+    void OnCollisionExit2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag == "Platform")
+        {
+            normal = coll.contacts[0].normal;
+            if (normal.x > 0 || normal.x < 0)
+                sideTouch = false;
+        }
+    }
+}
