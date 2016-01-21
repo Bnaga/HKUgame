397f72eb2f3312b57daeda92cfbd8789d00ee06f
Robbert Fliek <Bayangannaga@hotmail.com>
2016-01-21 23:21:39 +0100

    main menu and pause menu complete

--- HKU Game/Assets/Scripts/PauseMenu_script.cs
+++ HKU Game/Assets/Scripts/PauseMenu_script.cs
@@ -1,15 +1,43 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu_script : MonoBehaviour {
 
+
+    public bool isPaused;
+    public GameObject pauseMenuCanvas;
 	// Use this for initialization
 	void Start () {
 	
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update ()
+    {
+        if(isPaused)
+        {
+            pauseMenuCanvas.SetActive(true);
+            Time.timeScale = 0f;
+        }
+        else
+        {
+            pauseMenuCanvas.SetActive(false);
+            Time.timeScale = 1f;
+        }
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            isPaused = !isPaused;
+        }
 	}
+
+    public void Resume()
+    {
+        isPaused = false;
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(2);
+    }
 }
